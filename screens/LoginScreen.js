bd5323e36add7befeb213dfdd072314c280bbd03
Daltonic <darlingtongospel@gmail.com>
2021-10-27 14:31:06 +0100

    finishes login structure and style

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,17 +1,64 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, KeyboardAvoidingView } from 'react-native'
 import { Button, Input, Image } from 'react-native-elements'
 import { StatusBar } from 'expo-status-bar'
 
+const logo = require('../assets/logo.png')
+
 const LoginScreen = () => {
-    return (
-        <View>
-            <StatusBar style="light" />
-            <Text>I'm the login screen!</Text>
-        </View>
-    )
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const signIn = () => {}
+
+  return (
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
+      <StatusBar style="light" />
+      <Image source={logo} style={styles.ImageDimension} />
+      <View style={styles.inputContainer}>
+        <Input
+          placeholder="Email"
+          autoFocus
+          type="email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <Input
+          placeholder="Password"
+          secureTextEntry
+          type="password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+      </View>
+
+      <Button containerStyle={styles.button} onPress={signIn} title="Login" />
+      <Button containerStyle={styles.button} type="outline" title="Registers" />
+      <View style={{ height: 100 }} />
+    </KeyboardAvoidingView>
+  )
 }
 
 export default LoginScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  ImageDimension: {
+    width: 100,
+    height: 100,
+  },
+  inputContainer: {
+      width: 300,
+      marginVertical: 10
+  },
+  button: {
+      width: 200,
+      marginTop: 10
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    backgroundColor: 'white',
+  },
+})
